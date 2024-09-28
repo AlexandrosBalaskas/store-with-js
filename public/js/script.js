927619a1ba36849a0543c318b4ad9155b119d567
Alexandros Balaskas <98409867+AlexandrosBalaskas@users.noreply.github.com>
2024-09-28 19:49:22 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,10 @@ var homescreen = {
         var domProducts=`<div class='homeContainer'>`;
         let response = null ;
         if(urlparts[1]){
-            response = await fetch(`process.env.NODE_ENV === "production" ? "api/${urlparts[1]}" : http://localhost:4000/api/${urlparts[1]}`);
+            response = await fetch(process.env.NODE_ENV === "production" ? `api/${urlparts[1]}` : `http://localhost:4000/api/${urlparts[1]}`);
         }
         else{
-            response = await fetch(`process.env.NODE_ENV === "production" ? "api/products/ : http://localhost:4000/api/products/`);
+            response = await fetch(process.env.NODE_ENV === "production" ? `api/products/` : `http://localhost:4000/api/products/`);
         }
         const resource = await response.json();
         resource.forEach((element) => {
@@ -32,7 +32,7 @@ var productscreen = {
     render: async() => {
         const urli = window.location.hash;
         const urlparts = urli.split("/");
-        const response = await fetch(`process.env.NODE_ENV === "production" ? "api/${urlparts[2]}" : http://localhost:4000/api/products/${urlparts[2]}`);
+        const response = await fetch(process.env.NODE_ENV === "production" ? `api/${urlparts[2]}` : `http://localhost:4000/api/products/${urlparts[2]}`);
         const resource = await response.json();
         return `
         <div class="productContainer">
@@ -60,7 +60,7 @@ var cartscreen = {
         console.log(urli)
         if(urlparts[2])
         {
-        const response = await fetch(`process.env.NODE_ENV === "production" ? "api/${urlparts[2]}" : http://localhost:4000/api/products/${urlparts[2]}`);
+        const response = await fetch(process.env.NODE_ENV === "production" ? `api/${urlparts[2]}` : `http://localhost:4000/api/products/${urlparts[2]}`);
         const resource = await response.json();
         const product = JSON.stringify(resource);
         localStorage.setItem(resource.name,product);
